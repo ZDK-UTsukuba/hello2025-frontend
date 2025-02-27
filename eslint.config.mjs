441fd6f42e50ad38006ad2a4550574057925e053
shoga <yayoiclimbing@gmail.com>
2025-02-27 20:39:07 +0900

    add eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginAstro from "eslint-plugin-astro";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  pluginJs.configs.recommended,
+  eslintConfigPrettier,
+  ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs["jsx-a11y-recommended"],
+];
