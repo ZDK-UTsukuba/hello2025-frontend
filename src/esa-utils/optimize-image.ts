249ef9d03380cffa6626dd8a6dc7d2edd19d3423
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2025-03-27 00:14:26 +0900

    jsdocコメントを書いた

--- src/esa-utils/optimize-image.ts
+++ src/esa-utils/optimize-image.ts
@@ -1,5 +1,11 @@
 import { getImage } from "astro:assets";
 
+/**
+ * esa などから画像ファイルをダウンロードして最適化した画像に置き換えます。
+ * @param url esa などのURL https://img.esa.io/~
+ * @param low quality がlowで良い時はtrue
+ * @returns 内部の絶対パス /_astro/~
+ */
 export async function optimizeImage(
   url: string,
   low: boolean = false,
