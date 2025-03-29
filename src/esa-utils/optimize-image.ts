97c6f5970fdf8d210132461c47b9be7b3e4a446e
toririm <longtimedragon@icloud.com>
2025-03-30 00:44:55 +0900

    add JSDoc for optimize-image

--- src/esa-utils/optimize-image.ts
+++ src/esa-utils/optimize-image.ts
@@ -4,6 +4,8 @@ import { getImage } from "astro:assets";
  * esa などから画像ファイルをダウンロードして最適化した画像に置き換えます。
  * @param url esa などのURL https://img.esa.io/~
  * @param low quality がlowで良い時はtrue
+ * @param width 幅を指定する場合は指定
+ * @param height 高さを指定する場合は指定
  * @returns 内部の絶対パス /_astro/~
  */
 export async function optimizeImage(
@@ -22,7 +24,6 @@ export async function optimizeImage(
     width: width,
     height: height,
   });
-  console.log(img.options);
 
   return img.src;
 }
